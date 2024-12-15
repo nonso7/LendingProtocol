6cbf7315c0ddf8cc9919ca8c139d70903665e1bc
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-12-15 21:04:26 +0200

    Fixed testGetUsdValue()

--- script/DeployCuCoin.s.sol
+++ script/DeployCuCoin.s.sol
@@ -6,25 +6,46 @@ import {HelperConfig} from "./HelperConfig.sol";
 import {CuStableCoin} from "../src/CuStableCoin.sol";
 import {DefiProtocol} from "../src/CuCoinContract.sol";
 
+import {console} from "forge-std/Script.sol";
+
 contract DeployCuCoin is Script {
     address[] public tokenAddresses;
     address[] public priceFeedAddresses;
-    address initialOwner;
+    // address initialOwner;
     address auctionContract;
 
     function run() external returns (CuStableCoin, DefiProtocol, HelperConfig) {
         HelperConfig config = new HelperConfig(); // This comes with our mocks!
 
-        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, ) =
-        config.activeNetworkConfig();
+        (
+            address wethUsdPriceFeed,
+            address wbtcUsdPriceFeed,
+            address weth,
+            address wbtc,
+            uint256 deployerKey
+        ) = config.activeNetworkConfig();
+
+        console.log("weth", weth);
+        console.log("wbtc", wbtc);
+
+        console.log("wethUsdPriceFeed", wethUsdPriceFeed);
+        console.log("wbtcUsdPriceFeed", wbtcUsdPriceFeed);
+
         tokenAddresses = [weth, wbtc];
         priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
 
-        vm.startBroadcast();
-        CuStableCoin cu = new CuStableCoin(address(this));
-        DefiProtocol cud = new DefiProtocol(tokenAddresses, priceFeedAddresses, address(cu), auctionContract);
+        address deployer = vm.addr(deployerKey);
+        vm.startBroadcast(deployer);
+        CuStableCoin cu = new CuStableCoin(deployer);
+        DefiProtocol cud = new DefiProtocol(
+            tokenAddresses,
+            priceFeedAddresses,
+            address(cu),
+            auctionContract
+        );
 
-        cu.transferOwnership(initialOwner);
+        // cu.transferOwnership(initialOwner);
+        // initialOwner = msg.sender; // address(this)
         vm.stopBroadcast();
         return (cu, cud, config);
     }
